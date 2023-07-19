3e6689898edf3d64681b824f95e1ef72805a47d4
Hieulangtu <trunghieunguyen1905mu@gmail.com>
2023-07-19 11:15:36 +0200

    add comments

--- main.py
+++ main.py
@@ -12,13 +12,14 @@ print(pymongo.__version__)
 
 import base64
 
-
+#open the Chrome
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
 driver = webdriver.Chrome(options=options)
 #3 dòng trên là để mở cửa sổ mãi không đóng, 1 cách làm hay từ stackoverflow
 #Nếu viết như này : driver = webdriver.Chrome('chromedriver.exe') thì nó sẽ không chạy
 
+#open the link
 #mở đường link
 driver.get("https://vav.unob.cz/departments/index")
 sleep(8)
@@ -26,7 +27,7 @@ sleep(8)
 # print(base64.b64encode("password".encode("utf-8")))
 # print(base64.b64decode("cGFzc3dvcmQ=").decode("utf-8"))
 
-#Login part
+#Login part (decrypt login information in infor.txt)
 infor = open('infor.txt')
 lines=infor.readlines()
 username=base64.b64decode(lines[0]).decode("utf-8")
@@ -62,7 +63,7 @@ department_links = [department_elem.get_attribute("href") for department_elem in
 
 #đã lấy xong
 
-
+#function to get the links of each teacher in one department
 #hàm lấy link giáo viên ở 1 department
 def get_teacher_links_one_department(link_department):
     driver.get(link_department)
@@ -72,6 +73,7 @@ def get_teacher_links_one_department(link_department):
     teacher_links = [teacher_elem.get_attribute("href") for teacher_elem in teacher_elems]
     return teacher_links
 
+#Get all the links of all teachers and write to teacherlinks.txt
 #1 lấy hết các links giáo viên và viết vào teacherlinks.txt
 teacher_links = []
 for department_link in department_links:
@@ -91,7 +93,7 @@ else:
 
 # driver.get(f"https://{username}:{password}@apl.unob.cz/UOAPI/Rozvrhy/Osoba/243")
 
-
+#function to get CLASSES for teachers
 #hàm lấy lớp học cho giáo viên
 def get_classes(link_rozvrhy):
     path=link_rozvrhy.replace("https://", "")
@@ -108,6 +110,7 @@ def get_classes(link_rozvrhy):
           
     return classes
 
+#fuction to get INFORMATION of each teacher
 #và bây giờ là lấy thông tin 1 giáo viên / hàm lấy thông tin giáo viên
 def infor_teacher(teacher_link):
     driver.get(teacher_link)
@@ -136,6 +139,7 @@ def infor_teacher(teacher_link):
 #     teachers_data.append(teacher_infor)
 #1
 
+#Get teachers' informations
 #1 lấy toàn bộ thông tin giáo viên khi đã có links trong txt
 total_links = open('teacherlinks.txt')
 teacher_links_in_file=total_links.readlines()
@@ -150,22 +154,24 @@ for teacher_link in teacher_links_in_file:
 #     for teacher_data in teachers_data:
 #         f_t.write(str(teacher_data) + '\n')
 
-
+#write data to teacherdata.txt
 #1Viết thông tin vào teacherdata.txt nhưng đẹp hơn       
 with open("teacherdata.txt", "w", encoding="utf-8") as f:
     json.dump(teachers_data, f, indent = 4, ensure_ascii = False)   
 #1
 
+#write data to teacherdatas.json
 #1 viết vào file json
 with open("teacherdatas.json", "w", encoding="utf-8") as f:
     json.dump(teachers_data, f, indent = 4, ensure_ascii = False)
 #1
 
-
+#Write data to mongoDB
 # Đọc dữ liệu từ file JSON và ghi vào mongoDB
 with open("teacherdatas.json", "r",encoding="utf-8") as file:
     dat = json.load(file)
 
+#connect to MOngoDB server
 # Kết nối đến cơ sở dữ liệu MongoDB
 client = MongoClient("mongodb+srv://hieu-daren:19052000Hieu@clusterhieu.6lbtk6j.mongodb.net/?retryWrites=true&w=majority")
 # Lấy database mong muốn từ client
@@ -173,13 +179,14 @@ db = client["hieu1-csdl"]
 # Lấy collection mong muốn từ database
 collection = db["teachers"] 
 
-
+#add documents to collection
 # Thêm danh sách "a" vào collection
 if isinstance(dat, list):
     collection.insert_many(dat)
 else:
     collection.insert_one(dat)
 
+#close connect
 # Đóng kết nối tới cơ sở dữ liệu MongoDB
 client.close()
 driver.quit()
